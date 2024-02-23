6ea07523ba7b17185671670ddfe83f750d03755d
Victor Brenister <victorbrenister@vandebron.nl>
2024-02-23 16:58:47 +0100

    Init setup

---
+++ cmd/web/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func main() {
+	mux := http.NewServeMux()
+
+	fileServer := http.FileServer(http.Dir("./ui/static/"))
+
+	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
+
+	mux.HandleFunc("/", home)
+	mux.HandleFunc("/snippet/view", snippetView)
+	mux.HandleFunc("/snippet/create", snippetCreate)
+
+	log.Println("Staring server on :4000")
+
+	log.Panic(http.ListenAndServe(":4000", mux))
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/vbrenister/lets-go-snippetbox
+
+go 1.22.0
